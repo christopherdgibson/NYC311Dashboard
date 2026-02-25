6392e2fe2a07012e80cb776e87bec6b55796ce22
Chris <christopher.d.gibson1@gmail.com>
2026-02-25 17:29:27 +0100

    Add separate TableOptions #311Dashboard

---
+++ Services/Models/TableOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace NYC311Dashboard.Services.Models
+{
+    public class TableOptions<TItem>
+    {
+        public Func<TItem, string>? GroupBy { get; set; }
+        public string? TableStyles { get; set; }
+    }
+}
